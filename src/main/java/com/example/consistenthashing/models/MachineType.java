c688c78dd0ddc6ed023c38508f7173ff8d3a78db
Karanjot Singh <34736289+KJS1124@users.noreply.github.com>
2024-01-15 21:17:29 -0800

    Base Models (#1)

---
+++ src/main/java/com/example/consistenthashing/models/MachineType.java
@@ -0,0 +1,12 @@
+package com.example.consistenthashing.models;
+
+/**
+ * Author: Karanjot Singh
+ * User:karanjotsingh
+ * Date:2024-01-15
+ * Time:18:58
+ */
+public enum MachineType {
+    LEADER,
+    SLAVE
+}
---
+++ src/test/java/com/example/consistenthashing/ConsistentHashingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consistenthashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentHashingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
